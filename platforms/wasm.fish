d3d6526483b4b229bd7582e280c0aa0dbbfb703a
Moody <mooodyhunter@outlook.com>
2022-01-25 03:22:04 +0000

    add WASM support

---
+++ platforms/wasm.fish
@@ -0,0 +1,11 @@
+if [ "$EMSCRIPTEN_ROOT" = "" ]
+    set_color red
+    echo "Failed to determine Emscripten Root Path, please define 'EMSCRIPTEN_ROOT' environment variable."
+end
+
+set -p EXTRA_CMAKE_ARGUMENTS "-DCMAKE_TOOLCHAIN_FILE=$EMSCRIPTEN_ROOT/cmake/Modules/Platform/Emscripten.cmake"
+set -p EXTRA_CMAKE_ARGUMENTS "-DQT_HOST_PATH=$QT_HOST_PATH"
+
+# Neither QDoc nor clang-based lupdate needs to be built, so setting them off is safe.
+set -p EXTRA_CMAKE_ARGUMENTS "-DFEATURE_clangcpp=OFF"
+set -p EXTRA_CMAKE_ARGUMENTS "-DFEATURE_clang=OFF"
