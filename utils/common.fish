18a585e34cb4007f83af32b7895645aa87ae37e9
Moody <mooodyhunter@outlook.com>
2022-02-09 19:19:42 +0000

    add more modules

--- utils/common.fish
+++ utils/common.fish
@@ -6,6 +6,8 @@ set -g SRC_DIR "$BASE_DIR/qt"
 set -g QT_MODULES qtbase qtsvg qtshadertools qtimageformats \
     qtdeclarative qtmultimedia qt5compat qtlanguageserver \
     qtquicktimeline qtremoteobjects qttools qtactiveqt \
-    qttranslations qtwayland qtwebsockets
+    qttranslations qtwayland qtwebsockets qtquick3d qtpositioning \
+    qtcharts qtlottie qtnetworkauth qtvirtualkeyboard qtscxml \
+    qtserialbus qtserialport qtconnectivity
 
 cd $BASE_DIR
