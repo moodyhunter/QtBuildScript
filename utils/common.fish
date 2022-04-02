5d795349b4f8cfe55bc898595015500830b20e0c
Moody <mooodyhunter@outlook.com>
2022-04-02 15:58:10 +0100

    update

--- utils/common.fish
+++ utils/common.fish
@@ -21,11 +21,11 @@ function lastdedup --description 'Remove duplicates from variable'
     set $argv (reverse $newvar)
 end
 
-set -g BASE_DIR (realpath (cd (dirname (status -f))/../; and pwd))
+set -g BASE_DIR (/usr/bin/realpath (cd (dirname (status -f))/../; pwd))
 set -g SRC_DIR "$BASE_DIR/qt"
 
 set -g QT_MODULES qtbase qtsvg qtshadertools qtimageformats \
-    qtdeclarative qtmultimedia qt5compat qtlanguageserver \
+    qtdeclarative qt5compat qtlanguageserver \
     qtquicktimeline qtremoteobjects qttools qtactiveqt \
     qttranslations qtwayland qtwebsockets qtquick3d qtpositioning \
     qtcharts qtlottie qtnetworkauth qtvirtualkeyboard qtscxml \
