e467e3d4d3f8f3eb6703ceb472f9aa1ee216980c
Moody Liu <mooodyhunter@outlook.com>
2022-07-01 21:29:53 +0100

    use ANDROID_{S,N}DK_HOME instead of _PATH

--- utils/platforms/android.fish
+++ utils/platforms/android.fish
@@ -1,11 +1,11 @@
-if [ "$ANDROID_SDK_ROOT" = "" ]
+if [ "$ANDROID_SDK_HOME" = "" ]
     set_color red
-    echo "Failed to determine Android NDK Path, please define 'ANDROID_SDK_ROOT' environment variable."
+    echo "Failed to determine Android NDK Path, please define 'ANDROID_SDK_HOME' environment variable."
 end
 
-if [ "$ANDROID_NDK_ROOT" = "" ]
+if [ "$ANDROID_NDK_HOME" = "" ]
     set_color red
-    echo "Failed to determine Android NDK Path, please define 'ANDROID_NDK_ROOT' environment variable."
+    echo "Failed to determine Android NDK Path, please define 'ANDROID_NDK_HOME' environment variable."
 end
 
 set SUPPORTED_ANDROID_ARCH arm64-v8a armeabi-v7a x86 x86_64
@@ -24,11 +24,11 @@ if not contains -- "$QT_ARCH" $SUPPORTED_ANDROID_ARCH
     exit 1
 end
 
-set -p EXTRA_CMAKE_ARGUMENTS "-DANDROID_SDK_ROOT=$ANDROID_SDK_ROOT"
-set -p EXTRA_CMAKE_ARGUMENTS "-DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK_ROOT/build/cmake/android.toolchain.cmake"
+set -p EXTRA_CMAKE_ARGUMENTS "-DANDROID_SDK_ROOT=$ANDROID_SDK_HOME"
+set -p EXTRA_CMAKE_ARGUMENTS "-DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK_HOME/build/cmake/android.toolchain.cmake"
 set -p EXTRA_CMAKE_ARGUMENTS "-DQT_HOST_PATH=$QT_HOST_PATH"
 set -p EXTRA_CMAKE_ARGUMENTS "-DANDROID_PLATFORM=29"
 set -p EXTRA_CMAKE_ARGUMENTS "-DANDROID_ABI=$QT_ARCH"
 
-set -p EXTRA_EXPORT_VARIABLES ANDROID_NDK_ROOT
-set -p EXTRA_EXPORT_VARIABLES ANDROID_SDK_ROOT
+set -p EXTRA_EXPORT_VARIABLES ANDROID_NDK_HOME
+set -p EXTRA_EXPORT_VARIABLES ANDROID_SDK_HOME
