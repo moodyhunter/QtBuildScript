b5ddf4296e0365e14b43f306ddf651345f7c3f8d
Moody Liu <mooodyhunter@outlook.com>
2022-07-21 01:29:08 +0100

    test

--- utils/package.fish
+++ utils/package.fish
@@ -8,9 +8,11 @@ end
 
 cd $BASE_DIR/nightly
 
+ls
+
 for dir in (ls)
     cd $BASE_DIR/nightly
     set DATE_TO_PACKAGE (ls -d $dir/????-??-?? | sort -r | head -n1 | cut -d'/' -f2)
     set_color green && echo -n "=>" && set_color reset && echo " Packaging $dir ($DATE_TO_PACKAGE)..."
-    tar --zstd -cf "$BASE_DIR/nightly/.packaged/$dir-$DATE_TO_PACKAGE.tar.zst" "$dir/$DATE_TO_PACKAGE"
+    tar --gzip -cf "$BASE_DIR/nightly/.packaged/$dir-$DATE_TO_PACKAGE.tar.gz" "$dir/$DATE_TO_PACKAGE" || exit 1 
 end
